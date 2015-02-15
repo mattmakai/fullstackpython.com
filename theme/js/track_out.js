a7d9d0cd0c72f46a637cc3ae95f9b0bbe42e4b9c
Matt Makai <makaimc@gmail.com>
2015-02-15 11:56:07 -0500

    fixing broken js

--- theme/js/track_out.js
+++ theme/js/track_out.js
@@ -1,14 +1,3 @@
-<script>
-/**
-* Function that tracks a click on an outbound link in Google Analytics.
-* This function takes a valid URL string as an argument, and uses that URL string
-* as the event label.
-*/
 var trackOutboundLink = function(url) {
    ga('send', 'event', 'outbound', 'click', url, {'hitCallback':
-     function () {
-     document.location = url;
-     }
-   });
-}
-</script> 
+     function () { document.location = url; } }); }
